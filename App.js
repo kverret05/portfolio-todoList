e04462792faaf3534b751a1430319a3a1fe1e25c
kverret05 <katriav@gmail.com>
2023-04-21 18:52:53 -0400

    added removeTask

--- App.js
+++ App.js
@@ -18,8 +18,8 @@ async function cacheFonts(fonts) {
 }
 
 let initTasks = [
-  {description: "Do Homework", completed: true, key: 1, relatedTasks: [2] },
-  {description: "Take a shower", completed: true, key: 2, relatedTasks: [1]},
+  { description: "Do Homework", completed: true, key: 1, relatedTasks: [2] },
+  { description: "Take a shower", completed: true, key: 2, relatedTasks: [1] },
 ]
 
 const Stack = createNativeStackNavigator()
@@ -29,16 +29,16 @@ export default function App() {
     <NavigationContainer>
       <Tab.Navigator>
         <Tab.Screen
-        name="Home"
-        component = {ToDoHomeScreen}
-        options={{ headerShown: false }}
+          name="Home"
+          component={ToDoHomeScreen}
+          options={{ headerShown: false }}
         />
-        <Tab.Screen 
-        name="Settings" 
-        component={SettingsScreen} />
+        <Tab.Screen
+          name="Settings"
+          component={SettingsScreen} />
       </Tab.Navigator>
     </NavigationContainer>
-  )  
+  )
 }
 
 function SettingsScreen() {
@@ -60,138 +60,148 @@ function ToDoHomeScreen() {
         setTasks(JSON.parse(value))
       }
     }
-      getValue()
+    getValue()
   }, [])
 
-return <Stack.Navigator initialRouteName='To Do List'>
-<Stack.Screen name="Todo List">
-  {(props) => (
-    <TodoScreen 
-    {...props} tasks={tasks} setTasks={setTasks} />
-  )}
-</Stack.Screen>
-<Stack.Screen name="Details">
-  {(props) => (
-    <DetailsScreen
-     {...props} setTasks={setTasks} tasks={tasks} /> 
-  )}
-</Stack.Screen>
+  return <Stack.Navigator initialRouteName='To Do List'>
+    <Stack.Screen name="Todo List">
+      {(props) => (
+        <TodoScreen
+          {...props} tasks={tasks} setTasks={setTasks} />
+      )}
+    </Stack.Screen>
+    <Stack.Screen name="Details">
+      {(props) => (
+        <DetailsScreen
+          {...props} setTasks={setTasks} tasks={tasks} />
+      )}
+    </Stack.Screen>
   </Stack.Navigator>
 }
 
-function DetailsScreen({navigation, route, setTasks, tasks}) {
-    let {description, completed, key, relatedTasks} = route.params.item
-    useEffect(() => {
-        navigation.setOptions({
-        title: description === "" ? "No title" : description, 
-  })
+function DetailsScreen({ navigation, route, setTasks, tasks }) {
+  let { description, completed, key, relatedTasks } = route.params.item
+  useEffect(() => {
+    navigation.setOptions({
+      title: description === "" ? "No title" : description,
+    })
   }, [navigation])
   return (
-    <View style={{ flex: 1, alignItems: "center", justifyContent: "center"}}>
-    <Text>Details Screen New</Text>
-    <Text>{description}</Text>
-  {
- relatedTasks !== undefined && relatedTasks.length > 0 ?
-  <>
-    <Text>Related Tasks:</Text>
-      {tasks.filter(tasks= relatedTasks.includes(tasks.key))
-      .map(cTask => <Button key={cTask.key} title={cTask.description}
-       onPress={() => {
-         navigation.dispatch(StackActions.push('Details', {item:cTask}));
-      }}
-    />)
-  }
-  </>
-    : undefined}
-  </View>
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Text>Details Screen New</Text>
+      <Text>{description}</Text>
+      {
+        relatedTasks !== undefined && relatedTasks.length > 0 ?
+          <>
+            <Text>Related Tasks:</Text>
+            {tasks.filter(tasks = relatedTasks.includes(tasks.key))
+              .map(cTask => <Button key={cTask.key} title={cTask.description}
+                onPress={() => {
+                  navigation.dispatch(StackActions.push('Details', { item: cTask }));
+                }}
+              />)
+            }
+          </>
+          : undefined}
+    </View>
   )
+}
+
+function TodoScreen({ navigation, tasks, setTasks, removeTask }) {
+  cacheFonts([FontAwesome.font])
+  let [input, setInput] = useState("")
+  let updateTask = async (task) => {
+    console.log(task)
+    task.completed = !task.completed
+    setTasks([...tasks])
+    await AsyncStorage.setItem('@tasks', JSON.stringify(tasks))
+  }
+  let addTask = async () => {
+    let maxKey = 0
+    tasks.forEach(task => {
+      if (task.key > maxKey) {
+        maxKey = task.key
+      }
+    })
+
+    let newTasks = [
+      ...tasks,
+      {
+        description: input,
+        completed: false,
+        key: maxKey + 1,
+      },
+    ]
+
+    setTasks(newTasks)
+    console.log(newTasks)
+    await AsyncStorage.setItem('@tasks', JSON.stringify(newTasks))
+    setInput("")
   }
 
-  function TodoScreen({navigation, tasks, setTasks}) {
-    cacheFonts([FontAwesome.font])
-    let [input, setInput] = useState("")
-    let updateTask = async (task) => {
-      console.log(task)
-      task.completed = !task.completed
-      setTasks([...tasks])
-        await AsyncStorage.setItem('@tasks', JSON.stringify(tasks))
-    }
-    let addTask = async () => {
-    let maxKey = 0
-     tasks.forEach(task => {
-      if(task.key > maxKey) {
-        maxKey = task.key
-    }
-    })
-    let newTasks = [
-      ...tasks, 
-    {
-    description: input,
-    completed: false,
-    key: maxKey + 1,
-    },
-    ]
-    setTasks(newTasks)
-     console.log(newTasks)
-      await AsyncStorage.setItem('@tasks', JSON.stringify(newTasks))
-      setInput("")
-    }
-    let renderItem = ({item}) => {
-     return (
-      <View style={styles.horizontal}>
-    
-    <CheckBox
-    textStyle={item.completed ? {
-    textDecorationLine: "line-through",
-    textDecorationStyle: "solid",
-    } : undefined}
-    title={item.description}
-    checked={item.completed}
-    onPress={() => updateTask(item)}
-    />
-    <Button title="Details" onPress={() => navigation.navigate("Details", {item})}/>
-    </View>
-    )
-    
-    }
-    
+  let renderItem = ({ item }) => {
     return (
-      <View style={[styles.container]}>
-        <StatusBar style="auto" />
-          <FlatList data={tasks} renderItem={renderItem} />
-          <View style={[styles.horizontal]}>
-            <Input
-            onChangeText={setInput}
-            value={input}
-            placeholder="New task...">
-            </Input>
-            <Button title="Add task" onPress={addTask} />
-          </View>
+      <View style={styles.horizontal}>
+        <CheckBox
+          textStyle={item.completed ? {
+            textDecorationLine: "line-through",
+            textDecorationStyle: "solid",
+          } : undefined}
+          title={item.description}
+          checked={item.completed}
+          onPress={() => updateTask(item)}
+        />
+        <Button title="Details" onPress={() => navigation.navigate("Details", { item })} /> 
+        <Button title="Remove?" onPress={() => removeTask(item)} />
       </View>
     )
+
   }
 
-  const styles = StyleSheet.create({
-    image: {
-      flex: 1,
-      aspectRatio: 1,
-      width: '50%',
-      backgroundColor: '#0553',
-    },
-    horizontal: {
-      flexDirection: 'row',
-      justifyContent: 'space-around',
-      padding: 10,
-    },
-    container: {
-      flex: 1,
-      backgroundColor: '#fff',
-      alignItems: 'center',
-      justifyContent: 'center',
-    },
-    button: {
-      alignItems: 'center',
-      backgroundColor: '#DDDDDD',
-      padding: 10,
-    },
-  })
+  let removeTask = async (taskToRemove) => {
+    let newTasks = tasks.filter(task => task.key !== taskToRemove.key);
+    setTasks(newTasks);
+    console.log(newTasks);
+    await AsyncStorage.setItem('@tasks', JSON.stringify(newTasks));
+  }
+
+  return (
+    <View style={[styles.container]}>
+      <StatusBar style="auto" />
+      <FlatList data={tasks} renderItem={renderItem} />
+      <View style={[styles.horizontal]}>
+        <Input
+          onChangeText={setInput}
+          value={input}
+          placeholder="New task...">
+        </Input>
+        <Button title="Add task" onPress={addTask} />
+      </View>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  image: {
+    flex: 1,
+    aspectRatio: 1,
+    width: '50%',
+    backgroundColor: '#0553',
+  },
+  horizontal: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    padding: 10,
+  },
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  button: {
+    alignItems: 'center',
+    backgroundColor: '#DDDDDD',
+    padding: 10,
+  },
+})
