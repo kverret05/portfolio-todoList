9a5418f44d27b4bd7c254b5c500e210aaf090a30
Katriane Verret <97851573+kverret05@users.noreply.github.com>
2023-04-29 02:48:04 -0400

    Update SortTasks.js

--- src/components/SortTasks.js
+++ src/components/SortTasks.js
@@ -5,7 +5,7 @@
 
 // let taskData = [
   // let taskData = [
-  // { description: "Do Homework", completed: true, priority: "high" key: 1, relatedTasks: [2] },
-  // { description: "Play Video Games", completed: false, priority: "low" key: 2, relatedTasks: [3] },
+  // { description: "Do Homework", completed: true, priority: "high", key: 1, relatedTasks: [2] },
+  // { description: "Play Video Games", completed: false, priority: "low", key: 2, relatedTasks: [3] },
   // { description: "Take A Shower", completed: false, priority: "medium", key: 3, relatedTasks: [1] }
 // ]
