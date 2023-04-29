5cb9f42aa2b42442c7be7fbed262f13cbeb3a2de
Katriane Verret <97851573+kverret05@users.noreply.github.com>
2023-04-29 02:46:10 -0400

    Update SortTasks.js

--- src/components/SortTasks.js
+++ src/components/SortTasks.js
@@ -2,17 +2,8 @@
 // users can sort them through a dropdown menu
 // or, it could be a button called "Sort tasks by priority" and there can be a testID called "priority" that is associated with the array of tasks 
 // let taskData = [
-  // { 
-    // description: "Do homework", 
-    // completed: false, 
-    // key: 1, 
-    // priority: high
-    // relatedTasks: [2]
-    // },
-    //  { 
-    // description: "Play video games", 
-    // completed: false, 
-    // priority: low
-    // key: 2 
-    // },
-    // ];
+  // let taskData = [
+  // { description: "Do Homework", completed: true, priority: "high" key: 1, relatedTasks: [2] },
+  // { description: "Play Video Games", completed: false, priority: "low" key: 2, relatedTasks: [3] },
+  // { description: "Take A Shower", completed: false, priority: "medium", key: 3, relatedTasks: [1] }
+// ]
