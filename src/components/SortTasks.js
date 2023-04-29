09dc4234e38a40fd80ccd513c4461fdd6d38e7aa
Katriane Verret <97851573+kverret05@users.noreply.github.com>
2023-04-29 02:39:09 -0400

    Update SortTasks.js

--- src/components/SortTasks.js
+++ src/components/SortTasks.js
@@ -1,2 +1,3 @@
 // this component would consist of sorting tasks by priority: high, medium, and low
 // users can sort them through a dropdown menu
+// or, it could be a button called "Sort tasks by priority" and there can be a testID called "priority" that is associated with the array of tasks 
